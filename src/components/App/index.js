da6c30c5a7efca50d3f3c9e2b63845cbe4aa9ff5
giraudgabriel <gabriel.giraud@fatec.sp.gov.br>
2020-04-18 15:45:22 -0300

    #FEATURE: Inicializando as páginas.

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,10 +1,26 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom'
+import logo from '../../assets/logo.svg';
+import Button from '../Button';
 
 class App extends Component {
     render() {
         return (
-            <div>
-                App
+            <div className="ui center aligned container">
+                <div className="header">
+                    <h3>Fast Basket</h3>
+                    <img src={logo} className="ui small image centered aligned" alt="Fast Basket"/>
+                </div>
+                <div className="description">
+                    <h1>Crie agora mesmo seu catálogo virtual!</h1>
+                    <p>Cadastre seu catálogo online e compartilhe nas redes sociais</p>
+                    <Link to="/register">
+                        <Button className="green">Cadastre-se</Button>
+                    </Link>
+                    <Link to="/login">
+                        <Button className="low-green">Já tem uma conta? Faça login</Button>
+                    </Link>
+                </div>
             </div>
         );
     }
--- src/components/Register/index.js
+++ src/components/Register/index.js
@@ -1,10 +1,29 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+import logo from '../../assets/logo.svg';
+import Button from '../Button';
 
 class Register extends Component {
     render() {
         return (
-            <div>
-                Register
+            <div className="ui container">
+                <img src={logo} className="ui tiny image centered aligned" alt="Fast Basket"/>
+                <h3>Cadastre seu negócio</h3>
+                <p>Procurando organização? Fast Basket
+                    <br/>
+                    Cadastre e ganhe 1 mês de benefícios!
+                </p>
+                <input type="text" placeholder="Usuário"/>
+                <input type="text" placeholder="Senha"/>
+                <input type="text" placeholder="Whatsapp (com DDD)"/>
+                <Button className="green">Próximo</Button>
+                <h4>
+                    Já possui cadastro?
+                    <Link to="/login">
+                        <span></span>
+                        Entrar
+                    </Link>
+                </h4>
             </div>
         )
     }
