7776a8c7f6e0a691711ec28ae7a272bc8edae240
NicolasFujisawa <nicolau.fernando330@hotmail.com>
2020-04-19 12:09:10 -0300

    Responsive Product page, and App fix

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import logo from '../../assets/logo.svg';
+import './style.css';
 
 const Header = () => {
     return (
         <div className="ui small menu green">
             <Link className="item" to="/">
-                <img src={logo} alt="Fast Basket"/>
+                <img src={logo} alt="FastBasket"/>
             </Link>
+            <label className="title">FastBasket</label>
             <div className="right menu">
                 <div className="item">
                     <button className="ui icon button">
--- src/components/Product/index.js
+++ src/components/Product/index.js
@@ -1,10 +1,28 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
-
+import {Link} from 'react-router-dom';
+import './style.css'
 class Product extends Component {
     render() {
         return (
-            <div></div>
+            <div className="product-container">
+                <div className="content">
+                    <img src="" alt="Imagem do produto"/>
+                    <div className="toggether">
+                        <h3>Nome do Produto</h3>
+                        <p>Descrição do produto</p>
+                    </div>
+                    <div className="anotation">
+                        <p>Alguma observação?</p>
+                        <textarea type="" 
+                            placeholder="Descrição" style={{height: 104}}
+                        />
+                    </div>
+                    <Link to="/store/1">
+                        <button className="green" >Adicionar ao carrinho</button>
+                    </Link>
+                </div>
+            </div>
         )
     }
 }
